663516ac4c0510f7498bda089cc3e5654006cd4b
Oswaldoramirezb <oswaldo.ramirezbau@gmail.com>
2026-01-21 02:36:30 -0600

    clases de poo resumen

---
+++ Course-doc/175_poo_basico_resumen.py
@@ -0,0 +1,42 @@
+"""
+RESUMEN: POO BÁSICO
+===================
+Todas las operaciones básicas con POO
+"""
+
+print("=" * 50)
+print("RESUMEN - POO BÁSICO")
+print("=" * 50)
+
+print("\nDefinir clase:")
+print("  class MiClase:")
+print("      pass")
+
+print("\nConstructor:")
+print("  def __init__(self, param1, param2):")
+print("      self.param1 = param1")
+
+print("\nAtributos:")
+print("  self.atributo = valor  # Instancia")
+print("  atributo_clase = valor  # Clase")
+
+print("\nMétodos:")
+print("  def metodo(self):  # Instancia")
+print("  @classmethod")
+print("  def metodo_clase(cls):  # Clase")
+print("  @staticmethod")
+print("  def metodo_estatico():  # Estático")
+
+print("\nPropiedades:")
+print("  @property")
+print("  def propiedad(self):")
+print("      return self._valor")
+
+print("\nCrear objeto:")
+print("  objeto = MiClase(param1, param2)")
+
+print("\nAcceder:")
+print("  objeto.atributo")
+print("  objeto.metodo()")
+
+print("\n" + "=" * 50)
